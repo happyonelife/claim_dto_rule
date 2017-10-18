ae26f06b7bc56aaef51401a998bbfdda6a65ec3d
happyonelife <1403320782@qq.com>
2017-10-18 19:58:41 +0800

    提交文件

--- src/main/java/com/sinosoft/bpm/rule/bean/dto/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dto.bean.rule.bpm.sinosoft.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.sinosoft.bpm.rule.bean.dto;
